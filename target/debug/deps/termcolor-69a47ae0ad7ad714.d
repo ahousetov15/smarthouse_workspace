7c7acedae9e9955e2658a403770ac8320b161192
Хаустов Александр Дмитриевич <khaustovad@trcont.ru>
2024-05-26 20:45:17 +0300

    +3

--- target/debug/deps/termcolor-69a47ae0ad7ad714.d
+++
@@ -1,5 +0,0 @@
-/Users/house15/git/OTUS/RUST_DEVELOPER/smarthouse_workspace/target/debug/deps/libtermcolor-69a47ae0ad7ad714.rmeta: /Users/house15/.cargo/registry/src/index.crates.io-6f17d22bba15001f/termcolor-1.4.1/src/lib.rs
-
-/Users/house15/git/OTUS/RUST_DEVELOPER/smarthouse_workspace/target/debug/deps/termcolor-69a47ae0ad7ad714.d: /Users/house15/.cargo/registry/src/index.crates.io-6f17d22bba15001f/termcolor-1.4.1/src/lib.rs
-
-/Users/house15/.cargo/registry/src/index.crates.io-6f17d22bba15001f/termcolor-1.4.1/src/lib.rs:
